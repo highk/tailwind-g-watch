8bc411ba398691ae532cce0393c23c050e301893
superfast <restartplant@gmail.com>
2023-01-05 18:23:12 +0900

    Fix : tailwind.config.js typo

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const watch = require("../src/watch");
 
-if (fs.existsSync(`${process.cwd()}/taliwind.config.js`)) {
-  const config = require(`${process.cwd()}/taliwind.config.js`);
+if (fs.existsSync(`${process.cwd()}/tailwind.config.js`)) {
+  const config = require(`${process.cwd()}/tailwind.config.js`);
   watch(config);
 } else {
-  console.log(`${process.cwd()}/taliwind.config.js is not exist.`);
+  console.log(`${process.cwd()}/tailwind.config.js is not exist.`);
 }
