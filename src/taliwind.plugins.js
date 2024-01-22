a31002064c019fb0b494acd6fcfc12e8323b73c3
HiK <restartplant@gmail.com>
2024-01-22 17:15:51 +0900

    Update Packages

--- src/taliwind.plugins.js
+++ src/taliwind.plugins.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
-    require("@tailwindcss/line-clamp"),
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/container-queries"),
   ],
